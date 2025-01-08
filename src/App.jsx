092d05265d94c8fa85cf5fa8596094313f574ab5
magomedov-m <magomedov.dgu@gmail.com>
2025-01-08 15:46:16 +0300

    добавил стили css для красивого отображения

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,118 @@
-import { useEffect, useState } from 'react'
-import supabase from './supabase-client';
-
+import { useEffect, useState } from "react";
+import supabase from "./supabase-client";
+import "./App.css"; // Импортируйте ваш CSS файл
 
 function App() {
   const [todoList, setTodoList] = useState([]);
-  const [newTodo, setNewTodo] = useState('');
+  const [newTodo, setNewTodo] = useState("");
 
   useEffect(() => {
-    fetchTodo()
-  }, [])
+    fetchTodo();
+  }, []);
 
   const fetchTodo = async () => {
-    const {data, error} = await supabase.from('TodoList').select('*');
+    const { data, error } = await supabase.from("TodoList").select("*");
 
     if (error) {
-      console.log('error fetching:', error)
+      console.log("error fetching:", error);
     } else {
-      setTodoList(data)
+      setTodoList(data);
     }
-  }
+  };
 
   const completed = async (id, isCompleted) => {
-    const {data, error} = await supabase.from('TodoList').update({isCompleted: !isCompleted}).eq('id', id)
+    const { data, error } = await supabase
+      .from("TodoList")
+      .update({ isCompleted: !isCompleted })
+      .eq("id", id);
 
     if (error) {
-      console.log('error update:', error)
+      console.log("error update:", error);
     } else {
-      const update = todoList.map((todo) => todo.id === id ? {...todo, isCompleted: !isCompleted} : todo)
-      setTodoList(update)
+      const update = todoList.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: !isCompleted } : todo
+      );
+      setTodoList(update);
     }
-  }
+  };
 
   const addTodo = async () => {
     const newTodoData = {
       name: newTodo,
       isCompleted: false,
-    }
-    const {data, error} = await supabase.from('TodoList').insert([newTodoData]).single()
+    };
+    const { data, error } = await supabase
+      .from("TodoList")
+      .insert([newTodoData])
+      .single();
 
     if (error) {
-      console.log('error, adding todo:', error)
+      console.log("error, adding todo:", error);
     } else {
-      setTodoList((prev) => [...prev, data])
-      setNewTodo('')
+      setTodoList((prev) => [...prev, data]);
+      setNewTodo("");
     }
-  }
+  };
 
   const deleteTask = async (id) => {
-    const {data, error} = await supabase.from('TodoList').delete().eq('id', id)
+    const { data, error } = await supabase
+      .from("TodoList")
+      .delete()
+      .eq("id", id);
 
     if (error) {
-      console.log('error delete:', error)
+      console.log("error delete:", error);
     } else {
-      setTodoList((prev) => prev.filter((todo) => todo.id !== id))
+      setTodoList((prev) => prev.filter((todo) => todo.id !== id));
     }
-  }
+  };
 
   return (
-    <>
-      <h1>todo list</h1>
-      <div>
-        <input type='text' placeholder='новая задача...' value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
-        <button onClick={addTodo}>Добавить задачу</button>
+    <div className="app-container">
+      <h1 className="app-title">Todo List</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          className="todo-input"
+          placeholder="новая задача..."
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button className="add-button" onClick={addTodo}>
+          Добавить задачу
+        </button>
       </div>
 
-      <ul>
-        {
-          todoList.map((todo) => (
-            <li key={todo.id}>
-              <p>{todo.name}</p>
-              <button onClick={() => completed(todo.id, todo.isCompleted)}>{todo.isCompleted ? 'Выполнено' : 'Выполнить'}</button>
-              <button onClick={() => deleteTask(todo.id)}>Удалить задачу</button>
-            </li>
-          ))
-        }
+      <ul className="todo-list">
+        {todoList.map((todo) => (
+          <li
+            key={todo.id}
+            className={`todo-item ${todo.isCompleted ? "completed" : ""}`}
+          >
+            <p
+              className={`todo-checkbox ${todo.isCompleted ? "completed" : ""}`}
+            >
+              <div className="container_text">{todo.name}</div>
+            </p>
+            <div className="container_btn">
+              <button
+                className="add-button"
+                onClick={() => completed(todo.id, todo.isCompleted)}
+              >
+                {todo.isCompleted ? "Выполнено" : "Выполнить"}
+              </button>
+              <button
+                className="delete-button"
+                onClick={() => deleteTask(todo.id)}
+              >
+                Удалить задачу
+              </button>
+            </div>
+          </li>
+        ))}
       </ul>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
